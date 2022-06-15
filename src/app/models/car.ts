4ce9edb9b9b225527539fc72a94763af7909a7cd
Seda Ünal <sedaylmz06@outlook.com>
2022-06-15 12:30:10 +0300

    firts commit

--- src/app/models/car.ts
+++ src/app/models/car.ts
@@ -3,8 +3,8 @@ export class Car {
   brandId:number
   colorId:number
   dailyPrice:number
-  description:String
-  brandName:String
-  colorName:String
+  description:string
+  brandName:string
+  colorName:string
 }
 
